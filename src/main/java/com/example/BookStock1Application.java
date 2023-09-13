e6f616d1b0d9fe23562f5b175862a6dbebf636ed
chibiriku <96232950+chibiriku@users.noreply.github.com>
2023-09-13 14:15:12 +0900

    20230913-作成開始

---
+++ src/main/java/com/example/BookStock1Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStock1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStock1Application.class, args);
+	}
+
+}
